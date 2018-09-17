fb029c7128c3c4b96f1a2a9e2134401d0050f371
Michael <clarkmic@protonmail.com>
2018-09-17 11:57:07 -0400

    Added Enumerable for Monster Tiers and their respective XP ratings

--- MonsterMVC/Controllers/EncounterParamsController.cs
+++ MonsterMVC/Controllers/EncounterParamsController.cs
@@ -110,7 +110,7 @@ namespace MonsterMVC.Controllers
 
         public ActionResult GetAverageMonsterExperience(int totalExperienceAllowance, int numberOfMonstersInEncounter)
         {
-            var individualMonsterExperience =  totalExperienceAllowance / numberOfMonstersInEncounter;
+            var individualMonsterExperience =  Math.Round(totalExperienceAllowance / numberOfMonstersInEncounter);
             return RedirectToAction("GetTemporaryMonsters", new {individualMonsterExperience = individualMonsterExperience});
         }
 
